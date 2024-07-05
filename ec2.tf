63314f46b42810d64119d9daeea26a7ecde18334
dvillcar <dvillcar@contratista.epm.co>
2024-07-04 21:20:19 -0500

    ajuste con load balancer

--- ec2.tf
+++ ec2.tf
@@ -8,7 +8,7 @@ variable "ami" {
 
 variable "key_name" {
   description = "este el nombre de la key a usar ssh access"
-  default = "ec2-tf"
+  default = "ec2-tf-2"
 }
 
 resource "aws_instance" "web1" {
@@ -30,5 +30,59 @@ resource "aws_instance" "web2" {
   key_name = var.key_name
   tags = {
     Name = "ec2-tf-2"
+  }
+}
+
+resource "aws_lb" "lb_ec2" {
+  name = "lb-ec2"
+  internal = false
+  load_balancer_type = "application"
+  security_groups = [aws_security_group.lb_sg.id]
+  subnets = [aws_subnet.public1.id, aws_subnet.public2.id]
+  tags = {
+    Name = "lb-ec2"
+  }
+}
+
+resource "aws_lb_target_group" "target-group" {
+  name = "my-targets-lb"
+  port = 80
+  protocol = "HTTP"
+  vpc_id = aws_vpc.main.id
+  
+  health_check {
+    path = "/"
+    interval = 30
+    timeout = 5
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    matcher = "200"
+  }
+
+  tags = {
+    Name = "my-target-group"
+  }
+  
+}
+
+resource "aws_lb_target_group_attachment" "target-web1" {
+  target_group_arn = aws_lb_target_group.target-group.arn
+  target_id        = aws_instance.web1.id
+  port = 80
+}
+
+resource "aws_lb_target_group_attachment" "target-web2" {
+  target_group_arn = aws_lb_target_group.target-group.arn
+  target_id        = aws_instance.web2.id
+  port = 80
+}
+
+resource "aws_lb_listener" "http_lb" {
+  load_balancer_arn = aws_lb.lb_ec2.arn
+  port = 80
+  protocol = "HTTP"
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.target-group.arn
   }
 }
--- network.tf
+++ network.tf
@@ -133,3 +133,23 @@ resource "aws_security_group" "allow_ssh" {
     Name = "allow-ssh"
   }
 }
+
+resource "aws_security_group" "lb_sg" {
+  name        = "lb_sg"
+  description = "Allow traffic to the load balancer"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
